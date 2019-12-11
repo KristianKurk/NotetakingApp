055eb016546e4f2cded63b7961bd45617daf9876
nami <nami.sabet@hotmail.com>
2019-12-11 14:33:40 -0500

    Mom

--- NotetakingApp/MainWindow.xaml.cs
+++ NotetakingApp/MainWindow.xaml.cs
@@ -29,20 +29,50 @@ namespace NotetakingApp
 
         private void BtnClickMap(object sender, RoutedEventArgs e)
         {
+            DisableButton("mapNavButton");
             main.Content = new MappingWindow();
         }
         private void BtnClickNote(object sender, RoutedEventArgs e)
         {
+            DisableButton("noteNavButton");
             main.Content = new Note_takingWindow();
         }
         private void BtnClickRNG(object sender, RoutedEventArgs e)
         {
-            DisableButton("");
+            DisableButton("rngNavButton");
             main.Content = new RNGWindow();
         }
 
         private void DisableButton(string btn) {
 
+            EnableAll();
+
+            //Disable Selected button
+
+            object button = grid.FindName(btn);
+            Button button1 = (Button)button;
+            button1.IsEnabled = false;
+            button1.Background = new SolidColorBrush(Color.FromArgb(86, 50, 50, 0));
+        }
+        private void EnableAll() {
+
+            //Enable all buttons
+
+            object button1 = grid.FindName("noteNavButton");
+            object button2 = grid.FindName("rngNavButton");
+            object button3 = grid.FindName("mapNavButton");
+
+            Button navb1 = (Button)button1;
+            Button navb2 = (Button)button2;
+            Button navb3 = (Button)button3;
+
+            navb1.IsEnabled = true;
+            navb2.IsEnabled = true;
+            navb3.IsEnabled = true;
+
+            navb1.Background = new SolidColorBrush(Color.FromRgb(255, 193, 140));
+            navb2.Background = new SolidColorBrush(Color.FromRgb(255, 193, 140));
+            navb3.Background = new SolidColorBrush(Color.FromRgb(255, 193, 140));
         }
 
     }
