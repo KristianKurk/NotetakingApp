19b1c231ea88bbe8d84c8336485c71ab9ba4fe84
Kristian Kurktchiev <kristiankurk@gmail.com>
2019-12-12 15:27:10 -0500

    Initial pin creation (still not functioning 100%)

--- NotetakingApp/MappingWindow.xaml.cs
+++ NotetakingApp/MappingWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace NotetakingApp
     {
         private Matrix initialMat;
         private Point firstPoint = new Point();
+        private Point rightClickPoint = new Point();
 
         public MappingWindow()
         {
@@ -32,7 +33,6 @@ namespace NotetakingApp
             initialMat = cavRoot.RenderTransform.Value;
             BitmapImage img = DB.GetMap(1).LoadImage();
             imgSource.Source = img;
-            imgSource2.Source = img;
             init();
         }
 
@@ -45,10 +45,11 @@ namespace NotetakingApp
 
             imgSource.MouseRightButtonDown += (ss, ee) =>
             {
-                Console.WriteLine("Right Click X: " + ee.GetPosition(imgSource).X +" Right Click Y: "+ ee.GetPosition(imgSource).Y);
+                //Console.WriteLine("Right Click X: " + ee.GetPosition(imgSource).X +" Right Click Y: "+ ee.GetPosition(imgSource).Y);
                 ContextMenu cm = this.FindResource("cmButton") as ContextMenu;
                 cm.Placement = System.Windows.Controls.Primitives.PlacementMode.MousePoint;
                 cm.IsOpen = true;
+                rightClickPoint = ee.GetPosition(imgSource);
             };
 
             cavRoot.MouseWheel += (ss, ee) => {
@@ -86,6 +87,31 @@ namespace NotetakingApp
             cavRoot.MouseUp += (ss, ee) => {
                 firstPoint = new Point(-9999,-9999);
                 cavRoot.ReleaseMouseCapture(); };
+        }
+
+        private void Create_Pin_Click(object sender, RoutedEventArgs e)
+        {
+            int pin_size = 50;
+
+            Console.WriteLine("Created a pin");
+            Image pin = new Image();
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri("Assets/Pins/pin.png", UriKind.Relative);
+            bitmap.EndInit();
+            pin.Source = bitmap;
+            pin.Width = pin_size;
+            pin.Height = pin_size;
+
+            Canvas.SetTop(pin,rightClickPoint.Y);
+            Canvas.SetLeft(pin, rightClickPoint.X);
+
+            cavRoot.Children.Add(pin);
+        }
+
+        private void Create_Map_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Created a map");
         }
     }
 }
