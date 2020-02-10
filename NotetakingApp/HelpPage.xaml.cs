b99f410a5e5417e7af6fe95b314e894853db36cf
nami <f249master@F249-10>
2020-02-10 16:13:42 -0500

    HelpPage nav UI, HelpPageStyle updated, HelpPage navigation backend setup,

--- NotetakingApp/HelpPage.xaml.cs
+++ NotetakingApp/HelpPage.xaml.cs
@@ -20,9 +20,107 @@ namespace NotetakingApp
     /// </summary>
     public partial class HelpPage : Page
     {
+        String disabledButton;
         public HelpPage()
         {
             InitializeComponent();
+        }
+        private void BtnHelpDash(object sender, RoutedEventArgs e)
+        {
+            disabledButton = "helpDash";
+            DisableButton("helpDash");
+           // rng.Content = new RNGAdd();
+        }
+        private void BtnHelpMap(object sender, RoutedEventArgs e)
+        {
+            disabledButton = "helpMap";
+            DisableButton("helpMap");
+           // rng.Content = new RNGGenerate();
+        }
+        private void BtnHelpNote(object sender, RoutedEventArgs e)
+        {
+            disabledButton = "helpNote";
+            DisableButton("helpNote");
+          //  rng.Content = new RNGDice();
+        }
+        private void BtnHelpRNG(object sender, RoutedEventArgs e)
+        {
+            disabledButton = "helpRNG";
+            DisableButton("helpRNG");
+            //  rng.Content = new RNGDice();
+        }
+        private void BtnHelpSettings(object sender, RoutedEventArgs e)
+        {
+            disabledButton = "helpSettings";
+            DisableButton("helpSettings");
+            //  rng.Content = new RNGDice();
+        }
+        private void DisableButton(string btn)
+        {
+
+            EnableAll();
+
+            //Disable Selected button
+
+            object button = helpGrid.FindName(btn);
+            Button button1 = (Button)button;
+            button1.Background = new SolidColorBrush(Color.FromArgb(255, 86, 50, 50)) { Opacity = 0 };
+
+            //  button1.IsEnabled = false;
+            button1.Focusable = false;
+            button1.IsEnabled = false;
+
+        }
+        private void EnableAll()
+        {
+
+            //Enable all buttons
+
+            object button1 = helpGrid.FindName("helpDash");
+            object button2 = helpGrid.FindName("helpMap");
+            object button3 = helpGrid.FindName("helpNote");
+            object button4 = helpGrid.FindName("helpRNG");
+            object button5 = helpGrid.FindName("helpSettings");
+
+            Button navb1 = (Button)button1;
+            Button navb2 = (Button)button2;
+            Button navb3 = (Button)button3;
+            Button navb4 = (Button)button4;
+            Button navb5 = (Button)button5;
+
+            navb1.IsEnabled = true;
+            navb2.IsEnabled = true;
+            navb3.IsEnabled = true;
+            navb4.IsEnabled = true;
+            navb5.IsEnabled = true;
+
+            navb1.Background = new SolidColorBrush(Color.FromRgb(255, 229, 207)) { Opacity = 1 };
+            navb2.Background = new SolidColorBrush(Color.FromRgb(255, 229, 207)) { Opacity = 1 };
+            navb3.Background = new SolidColorBrush(Color.FromRgb(255, 229, 207)) { Opacity = 1 };
+            navb4.Background = new SolidColorBrush(Color.FromRgb(255, 229, 207)) { Opacity = 1 };
+            navb5.Background = new SolidColorBrush(Color.FromRgb(255, 229, 207)) { Opacity = 1 };
+
+        }
+        //Set button hover color
+        private void rngNavButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+            Button button1 = (Button)sender;
+            if (disabledButton != button1.Name)
+            {
+                button1.Background = new SolidColorBrush(Color.FromRgb(250, 238, 227));
+            }
+        }
+
+        //Reset button hover color on mouse leave
+        private void rngNavButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button button1 = (Button)sender;
+
+            if (disabledButton != button1.Name)
+            {
+                button1.Background = new SolidColorBrush(Color.FromRgb(255, 229, 207));
+            }
         }
     }
 }
